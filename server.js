a4bada8419bb456994e6fdd1995638c0ccd86066
AmirHPartovi <a.partovi99@gmail.com>
2023-04-28 09:43:12 +0330

    cluster & workers set up

--- server.js
+++ server.js
@@ -10,13 +10,23 @@ const delay =(time)=>{
     }
 }
 app.get('/',(req,res)=>{
-    res.status(200).send('none block opp ...')
+    res.status(200).send(`none-block opp ...and process id = ${process.pid}`)
 })
 app.get('/timer',(req,res)=>{
     delay(9000);
-    res.status(200).send('ding ding ding ...')
+    res.status(200).send(`block opp => ding ding ding ... and process id = ${process.pid}`)
 })
+const cpuCore = os.cpus().length;
 
 
+console.log('it shows 10(8worker + 1master + 1before) times')
+if(cluster.isMaster){
+    console.log(`master has been started`)
+    for(let i=0 ; i<cpuCore ;i++){
+        cluster.fork();
+    }
+}else{
+    app.listen(3000);
+    console.log(`worker started`)
+}
 
-app.listen(3000);
